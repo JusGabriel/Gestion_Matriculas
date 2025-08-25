655e3c60cb32d6a2a7c5ee0303d3021a29765dfe
JusGabriel <168014809+JusGabriel@users.noreply.github.com>
2025-08-25 17:46:12 -0500

    Update index.js

--- Ges-Matriculas/src/index.js
+++ Ges-Matriculas/src/index.js
@@ -1,22 +1,14 @@
 import app from './server.js';
-import connectDB from './database.js';
+import connection from './database.js';
 import dotenv from 'dotenv';
 
-dotenv.config(); // Cargar variables de entorno
+dotenv.config();
 
-// Configurar puerto
 const PORT = process.env.PORT || 3000;
 app.set('port', PORT);
 
-// Conectar a la base de datos
-connectDB()
-  .then(() => {
-    // Iniciar servidor
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error('Error connecting to the database:', err);
-    process.exit(1); // Salir si falla la conexión
+connection().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
+});
